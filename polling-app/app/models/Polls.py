938bb5cfefa16cb7617fbf4062d0aff4ae8824d0
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-02-07 17:12:21 +0200

    35. Poll vs PollCreate

--- polling-app/app/models/Polls.py
+++ polling-app/app/models/Polls.py
@@ -2,13 +2,18 @@ from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
 from typing import List, Optional
 from datetime import datetime, timezone
+from Choice import Choice
 
-
-class Poll(BaseModel):
-    id: UUID = Field(default_factory=uuid4)
+class PollCreate(BaseModel):
+    """Poll creating data model"""
     title: str = Field(min_length=5, max_length=50)
     options: List[str]
+    expires_at: Optional[datetime] = None
+
+class Poll(PollCreate):
+    """Poll data model"""
+    id: UUID = Field(default_factory=uuid4)
+    options: List[Choice]
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc)
     )
-    expires_at: Optional[datetime] = None
