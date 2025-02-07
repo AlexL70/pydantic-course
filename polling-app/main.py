d90136524e3e018412fadaf7f3a91c97f6ef1692
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-02-07 16:01:51 +0200

    31. Polls Create With Placeholders

--- polling-app/main.py
+++ polling-app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import ValidationError
+from app.models.Polls import Poll
+from typing import List
 
 app = FastAPI()
 
 @app.get("/test")
 def test():
     return {"message": "Hello there!"}
+
+@app.post("/polls/create")
+def create_poll():
+    try:
+        poll = Poll(options=["First", "Second", "Third"])
+        return poll
+    except ValidationError as e:
+        return e
