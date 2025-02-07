baee63c328a31276d3413efc3ebc0ecc65296959
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-02-07 16:42:55 +0200

    34. Splitting Into Read And Write Models

--- polling-app/app/models/Choice.py
+++ polling-app/app/models/Choice.py
@@ -1,7 +1,11 @@
 from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
 
-class Choice(BaseModel):
-    id: UUID = Field(default_factory=uuid4)
+class ChoiceCreate(BaseModel):
+    """Model for creating a new choice"""
     description: str = Field(min_length=1, max_length=100)
+
+class Choice(ChoiceCreate):
+    """Model representing a single choice in a poll"""
+    id: UUID = Field(default_factory=uuid4)
     label: int = Field(ge=1, le=5)
