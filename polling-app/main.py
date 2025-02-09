92cdcfc7e5410fcc424a5097f8822ea0042b958d
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-02-09 15:10:40 +0200

    42. Connecting, Saving, And Retrieving Data From Redis

--- polling-app/main.py
+++ polling-app/main.py
@@ -19,3 +19,24 @@ def create_poll(poll: PollCreate):
         }
     except ValidationError as e:
         return e
+
+        
+### explore and throw away ###
+from redis import Redis
+redis_client = Redis(
+    host = "127.0.0.1",
+    port = 6379,
+    password = "",
+    db = 0
+)
+redis_client.ping()
+
+@app.post("/redis/save")
+def save_redis(id: str, name: str):
+    redis_client.set(id, name)
+    return {"status": "success"}
+
+@app.get("/redis/get/{id}")
+def retrieve_redis(id: str):
+    result = redis_client.get(id)
+    return {"result": result}
